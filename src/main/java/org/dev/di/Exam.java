4d64ac3585804936155931b8eb2ed222bab2f932
Rakesh9556 <Rakeshguru9556@gmail.com>
2024-12-30 10:20:20 +0530

    project updated

--- src/main/java/org/dev/di/Exam.java
+++ src/main/java/org/dev/di/Exam.java
@@ -8,15 +8,8 @@ public class Exam {
     public static void main(String[] args) {
 
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-
         Student student1 = context.getBean("student1", Student.class);
-
-        // This will inject studentName dependency using setter method
-        // student.setStudentName("Rakesh G");
         student1.displayStudentInfo();
-
-        Student student2 = context.getBean("student2", Student.class);
-        student2.displayStudentInfo();
 
     }
 }
