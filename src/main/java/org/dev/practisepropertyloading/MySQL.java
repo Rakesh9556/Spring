622955bde2571ae0f07f0cd4ba8f24e82412dff1
Rakesh9556 <Rakeshguru9556@gmail.com>
2025-01-10 12:03:10 +0530

    practised data loading from property file using database conn example

---
+++ src/main/java/org/dev/practisepropertyloading/MySQL.java
@@ -0,0 +1,39 @@
+package org.dev.practisepropertyloading;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class MySQL {
+
+    @Value("${mysql.driver}")
+    private String driver;
+
+    @Value("${mysql.url}")
+    private String url;
+
+    @Value("${mysql.username}")
+    private String username;
+
+    @Value("${mysql.password}")
+    private String password;
+
+    public String getDriver() {
+        return driver;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void displayDatabaseInfo() {
+        System.out.println("Driver: " + driver + " URL: " + url + " Username: " + username + " Password: " + password);
+    }
+
+}
